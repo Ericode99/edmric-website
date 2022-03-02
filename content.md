a0b5a19c6cdbce8f99071046a548b776cd304473
Ericode99 <eric.rudischhauser@gmail.com>
2022-03-02 16:28:28 +0100

    inital commit

---
+++ content.md
@@ -0,0 +1,36 @@
+# EDMric
+
+### EDMric 2021
+
+EDMric 2021 is EDMric's first released and most recent album. It launched in December 2021 and features EDM songs wich EDMric produced all over 2021. The image on the cover was taken by EDMric on his favourite beach in Mexico.
+
+Listen on
+
+Spotify
+Apple Music
+ITunes
+Soundcloud
+Youtube
+Deezer
+
+### About EDMric
+
+EDMric always liked music. As a child he played the piano and in high school he sang in the choir. He also produced some very simple remixes of popular pop songs and took a dj course.
+
+EDMric loves EDM songs and listenes to EDM all the time when he's at the gym, doing chores or driving the car. But in his opinion there are too few EDM songs that he relly likes, wich is why he started producing some himself.
+
+### The EDM Genre
+
+EDM stands for Electronix dance music and is made largely for nightclubs, raves, and festivals. Despite the music industries attemt to create a specific EDM brand, it remains in use as an umbrella term for multiple genres, including dance-pop, house, techno and electro, as well as their respective subgenres.
+
+Following the emergence of raving, pirate radio, PartyCrews, underground festivals and an upsurge of interest in club culture during the late 1980s and early 1990s, EDM achived mainstream popularity in Europe.
+
+In the United States however, the government tryed to halt the spread of rave culture due to it being linked to drug use. It was not until the early 2000s that EDM rose to mainstream popularity in the US.
+
+## Copiright
+
+Header, Nav
+EDMric 2021
+About EDMric
+The EDM Genre
+Footer
---
+++ script.js
@@ -0,0 +1,89 @@
+/////////////////////////////////////////////////////////////
+// MOBILE NAVIGATION
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".section-header");
+const navItemEl = document.querySelectorAll(".nav-item");
+
+// Open/close mobile nav
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+// Close mobile nav after link click
+navItemEl.forEach((El) =>
+  El.addEventListener("click", function () {
+    headerEl.classList.remove("nav-open");
+  })
+);
+
+/////////////////////////////////////////////////////////////
+// Smooth scrolling animation
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+  });
+});
+
+///////////////////////////////////////////////////////////
+// Sticky navigation
+
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0.3,
+    rootMargin: "",
+  }
+);
+obs.observe(sectionHeroEl);
+
+///////////////////////////////////////////////////////////
+// Fixing flexbox gap property missing in old Safari versions
+function checkFlexGap() {
+  var flex = document.createElement("div");
+  flex.style.display = "flex";
+  flex.style.flexDirection = "column";
+  flex.style.rowGap = "1px";
+
+  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement("div"));
+
+  document.body.appendChild(flex);
+  var isSupported = flex.scrollHeight === 1;
+  flex.parentNode.removeChild(flex);
+  console.log(isSupported);
+
+  if (!isSupported) document.body.classList.add("no-flexbox-gap");
+}
+checkFlexGap();
